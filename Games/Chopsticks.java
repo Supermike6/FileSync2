6fc3f3383beea374e9eb6feda0a8cbff58da8606
Supermike6 <gbancroft25@pascack.org>
2022-10-13 12:08:35 -0400

    Create Chopsticks.java

---
+++ Games/Chopsticks.java
@@ -0,0 +1,18 @@
+package Games;
+
+import java.util.Scanner;
+
+public class Chopsticks
+{
+    public static void main(String[] args)
+    {
+        String mode = "";
+        Scanner sc = new Scanner(System.in);
+        int p11 = 1;
+        System.out.println("P1: ");
+        String input = sc.nextLine();
+
+
+
+    }    
+}
