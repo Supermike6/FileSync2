232b959c25f61015df43997085f34d120f749947
Supermike6 <gbancroft25@pascack.org>
2022-10-20 17:17:49 -0400

    fixed vscode not liking something

--- Games/BrickBrakeFolder/BrickBraker.java
+++ Games/BrickBrakeFolder/BrickBraker.java
@@ -1,4 +1,4 @@
-package Games.BrickBrakeFolder;
+package BrickBrakeFolder;
 
 import java.awt.Color;
 import java.awt.Dimension;
---
+++ Games/Chopsticks.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Chopsticks
+{
+    private int p11 = 0;
+    private int p12 = 0;
+    private int p21 = 0;
+    private int p22 = 0;
+    private String mode = "default";
+    public static void main(String[] args)
+    {
+        Scanner sc1 = new Scanner(System.in);
+        while
+    }
+}
