afe20b62b6296ce0bdd8b1583c93b9a1574c82bd
Supermike6 <gbancroft25@pascack.org>
2022-11-09 15:41:49 -0500

    stuff from compsci

--- Experiments/math/Desmosus.java
+++ Experiments/math/Desmosus.java
@@ -1,16 +1,38 @@
 package Experiments.math;
 
+import java.util.ArrayList;
+
 public class Desmosus
 {
     public static void main(String[] args)
     {
-        String eq = "y=x";
+        String eq = "20=5x";
         int yLoc = eq.indexOf("y");
         int eLoc = eq.indexOf("=");
+        int xLoc = eq.indexOf("x");
         String leftSide = eq.substring(0, eLoc);
-        String rightSide = eq.substring(eLoc);
+        String rightSide = eq.substring(eLoc+1);
+        System.out.println(leftSide+", "+rightSide);
+        for(String step:breakNumbers(eq)) System.out.println(step);
+    }
+    private static ArrayList<String> breakNumbers(String eq)
+    {
+        ArrayList<String> out = new ArrayList<String>();
+        for(int i = 0;i<eq.length();i++)
+        {
+            out.add(eq.substring(i, eq.indexOf("+")));
+            out.add(eq.substring(i, eq.indexOf("-")));
+            out.add(eq.substring(i, eq.indexOf("*")));
+            out.add(eq.substring(i, eq.indexOf("/")));
+            out.add(eq.substring(i, eq.indexOf("=")));
+            out.add(eq.substring(i, eq.indexOf("(")));
+            out.add(eq.substring(i, eq.indexOf(")")));
+        }
 
 
-    }    
+        return out;
+    }
+
+     
 
 }
