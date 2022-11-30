22e0adca537377db64cc5ea5e5c3f4af750d8e61
Supermike6 <gbancroft25@pascack.org>
2022-11-30 15:50:53 -0500

    Create jeoprody.py

---
+++ notJava/Python/jeoprody.py
@@ -0,0 +1,5 @@
+import requests as req
+
+questions=req.get('https://jservice.io/api/clues?value=100&min_date=1985-02-20').json()
+for i in range(0,10):
+    print(f"question {i}:"+questions[i]["question"])
