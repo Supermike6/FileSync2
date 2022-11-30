49335f0b57afb1bd00ad57a2b9af8a45ab38d74a
Supermike6 <gbancroft25@pascack.org>
2022-11-30 09:17:09 -0500

    3d time

---
+++ Experiments/Graphics/ddd/cube.java
@@ -0,0 +1,25 @@
+package Experiments.Graphics.ddd;
+
+public class cube
+{
+    int l;
+    int w;
+    int h;
+    int x;
+    int y;
+    int z;
+    cube(int length, int width, int height, int x, int y, int z)
+    {
+        length = this.l;
+        width = this.w;
+        height = this.h;
+        x = this.x;
+        y = this.y;
+        z = this.z;
+    }
+
+    @Override
+    public String toString() {
+        return "["+
+    }
+}
---
+++ Experiments/Graphics/ddd/wireframe.java
@@ -0,0 +1,16 @@
+package Experiments.Graphics.ddd;
+
+import java.util.ArrayList;
+
+import Experiments.Graphics.ddd.cube;
+
+public class wireframe 
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(69);
+        cube c = new cube(1, 2, 3, 4, 5, 6);
+        System.out.println(nums);
+    }
+}
