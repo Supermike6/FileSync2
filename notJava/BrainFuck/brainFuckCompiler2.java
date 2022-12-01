3bc51a5b9b69c728bfbe3431be676be0367a2be1
Supermike6 <gbancroft25@pascack.org>
2022-12-01 08:59:55 -0500

    WORKING BRAINFUCK

--- notJava/BrainFuck/brainFuckCompiler2.java
+++ notJava/BrainFuck/brainFuckCompiler2.java
@@ -7,85 +7,34 @@ import java.util.Scanner;
 
 public class brainFuckCompiler2 
 {
-    // private static String line;
-    // private static Integer[] memory = new Integer[256];
-    // private static int memoryHead = 0;
-    // private static ArrayList<String> instructions = new ArrayList<>(0);
-    // private static String instruction = "";
-    // private static Scanner sc = new Scanner(System.in);
+    static String line="";
+    static Integer[] memory = new Integer[32];
+    static int memoryHead = 0;
+    static ArrayList<String> instructions = new ArrayList<>(0);
+    String instruction = "";
     public static void main(String[] args) throws FileNotFoundException
     {
-        //initializing variables
-        String line="";
-        Integer[] memory = new Integer[256];
-        int memoryHead = 0;
-        ArrayList<String> instructions = new ArrayList<>(0);
-        String instruction = "";
         File file = new File("notJava/BrainFuck/testcode.bf");
         Scanner sc = new Scanner(file);
-
+        //initializing variables
+        
         //populating memory array
-        for(int i = 0; i<256; i++)
+        for(int i = 0; i<32; i++)
         {
             memory[i] = 0;
         }
-    
+        
         //populating instructions list
         while(sc.hasNextLine())
         {
-            line = sc.nextLine();
-            for(int i = 0; i<line.length();i++){
-                instructions.add(line.split("")[i]);
-            }
+            line += sc.nextLine();
         }
-
-        compipleAndRun(memory, memoryHead, instructions);
-
+        runTask(line);
+        
         //display the Memory Array
-        printMemory(memory, memoryHead);
-    
+        // printMemory(memory, memoryHead);
+        
         System.out.println();
-    }
-
-    public static void compipleAndRun(Integer[] memory, int memoryHead, ArrayList<String> instructions) throws FileNotFoundException
-    {
-        String instruction = "";
-        Scanner sc = new Scanner(System.in);
-    //basic instructions loop
-        while(instructions.size()>0)
-        {
-            instruction = instructions.get(0);
-            if(instruction.equals("+")){
-                memory[memoryHead]++;
-                if(memory[memoryHead]>255)
-                {
-                    memory[memoryHead] = 0;
-                }
-            }
-            if(instruction.equals("-")){
-                memory[memoryHead]--;
-                if(memory[memoryHead]<0)
-                {
-                    memory[memoryHead] = 255;
-                }
-            }
-            if(instruction.equals(">")){
-                memoryHead++;
-            }
-            if(instruction.equals("<")){
-                memoryHead--;
-            }
-            if(instruction.equals(".")){
-                System.out.print((char)(int)memory[memoryHead]);
-            }
-            if(instruction.equals(",")){
-                System.out.print("Enter a character: ");
-                sc = new Scanner(System.in);
-                memory[memoryHead] = sc.nextInt();
-            }
-            looping(instructions, memory, memoryHead, sc);
-            instructions.remove(0);
-        }
     }
     public static void printMemory(Integer[] memory, int memoryHead)
     {
@@ -107,6 +56,7 @@ public class brainFuckCompiler2
         for(int i = 0; i<32; i++) {
             System.out.print(tempList.get(i)+" ");
         }
+        System.out.println();
     }
     
     public static int digits(int num)
@@ -119,25 +69,67 @@ public class brainFuckCompiler2
         }
         return count;
     }
-
-    public static void looping(ArrayList<String> instructions, Integer[] memory, int memoryHead, Scanner sc) throws FileNotFoundException
+    
+    public static void runTask(String in)
     {
-
-        int tempInt = memory[memoryHead];
-        int tempMemH = memoryHead;
-        if(instructions.get(0).equals("["))
-        System.out.println(tempInt);
-        String instS = "";
-        for(String s: instructions) instS+=s;
-        System.out.println(instS);
-        while(tempInt!=0)
+        int instIndex = 0;
+        int startHead = 0;
+        char[] insts = in.toCharArray();
+        for(int i = 0;i<insts.length;i++)
         {
-            compipleAndRun(memory, memoryHead, instructions);
-            tempInt = memory[memoryHead];
-            tempMemH = memoryHead;
-        }
-        
-        
-
+            char inst = insts[i];
+            if(inst=='+')
+            {
+                memory[memoryHead]++;
+                if(memory[memoryHead]>255) memory[memoryHead]=0;   
+            }
+            if(inst=='-')
+            {
+                memory[memoryHead]--;
+                if(memory[memoryHead]<0) memory[memoryHead]=255;  
+            }
+            if(inst=='>')
+            {
+                memoryHead ++;
+            }
+            if(inst=='<')
+            {
+                if(memoryHead-1<0)
+                {
+                    memoryHead = 31;
+                } else {
+                    memoryHead --;
+                }   
+            }
+            if(inst=='.')
+            {
+                System.out.print((char)(int)memory[memoryHead]);
+            }
+            if(inst==',')
+            {
+                Scanner sc = new Scanner(System.in);
+                memory[memoryHead] = (int) sc.nextLine().toCharArray()[0];
+            }
+            // in.substring(instIndex, in.indexOf("]", instIndex));
+            
+            if(inst == '[')
+            {
+                startHead=memoryHead;
+                if(memory[startHead] == 0)
+                {
+                    for(int j = 0; j<in.substring(i,in.indexOf("]", i)).length(); j++)
+                        i++;
+                } else 
+                {
+                    while(memory[startHead] !=0)
+                    {
+                        runTask(in.substring(i+1,in.indexOf("]", i+1)));
+                    }
+                    // System.out.println(memory[startHead]);
+                    i = in.indexOf("]", i);
+                }
+            }
+            // printMemory(memory, memoryHead);
+        }   
     }
 }
