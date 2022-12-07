2a5dd5334a1ac1c1620df0bd0384daf3bf0e3a66
Supermike6 <gbancroft25@pascack.org>
2022-12-07 14:47:58 -0500

    buf works

--- Experiments/Random/random.java
+++ Experiments/Random/random.java
@@ -6,18 +6,8 @@ public class random
 {
     public static void main(String[] args)
     {
+
         
-        int[] arr = {10, 20, 30, 40, 50};
 
-for(int x = 1; x < arr.length - 1; x++)
-
-{
-
-arr[x + 1] = arr[x] + arr[x + 1];
-
-}
-for(int i : arr)System.out.println(i);
-        
-        
     }
 }
