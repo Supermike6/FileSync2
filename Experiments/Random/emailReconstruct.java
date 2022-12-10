3ebbe486fddce760c328411ea5895cba59a6e343
Supermike6 <gbancroft25@pascack.org>
2022-12-10 13:19:39 -0500

    Create emailReconstruct.java

---
+++ Experiments/Random/emailReconstruct.java
@@ -0,0 +1,9 @@
+package Experiments.Random;
+
+public class emailReconstruct
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello i am setup right ");
+    }    
+}
