0ed604e7d466c7a4e8f564a347d118022fb4ac54
Supermike6 <gbancroft25@pascack.org>
2022-12-16 13:20:55 -0500

    stuff

---
+++ notJava/BrainFuck/brainFuckCompiler4.java
@@ -0,0 +1,70 @@
+package notJava.BrainFuck;
+
+import java.util.Scanner;
+
+public class brainFuckCompiler4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the BrainFuck code from the input
+        String code = scanner.nextLine();
+
+        // Initialize the memory tape and pointer
+        int[] tape = new int[30000];
+        int pointer = 0;
+
+        // Iterate through each character in the code
+        for (int i = 0; i < code.length(); i++) {
+            char c = code.charAt(i);
+
+            switch (c) {
+                case '>':
+                    pointer++;
+                    break;
+                case '<':
+                    pointer--;
+                    break;
+                case '+':
+                    tape[pointer]++;
+                    break;
+                case '-':
+                    tape[pointer]--;
+                    break;
+                case '.':
+                    System.out.print((char) tape[pointer]);
+                    break;
+                case ',':
+                    tape[pointer] = scanner.next().charAt(0);
+                    break;
+                case '[':
+                    if (tape[pointer] == 0) {
+                        int loopCount = 1;
+                        while (loopCount > 0) {
+                            i++;
+                            char d = code.charAt(i);
+                            if (d == '[') {
+                                loopCount++;
+                            } else if (d == ']') {
+                                loopCount--;
+                            }
+                        }
+                    }
+                    break;
+                case ']':
+                    if (tape[pointer] != 0) {
+                        int loopCount = 1;
+                        while (loopCount > 0) {
+                            i--;
+                            char d = code.charAt(i);
+                            if (d == ']') {
+                                loopCount++;
+                            } else if (d == '[') {
+                                loopCount--;
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
